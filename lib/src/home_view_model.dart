fb1980975f4efb2d39d23f87fdb27d338438ba38
Andrey Chernoprudov <achernoprudov@naumen.ru>
2019-05-12 23:47:03 +0500

    ticker viewModel logic

--- lib/src/home_view_model.dart
+++ lib/src/home_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:scoped_model/scoped_model.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +8,12 @@ class HomeViewModel extends Model {
   final _pageChangeCurve = Curves.fastOutSlowIn;
 
   var pageController = PageController();
+  Stream<int> secondsStream = Stream.empty();
 
+  StreamSubscription<int> _secondsListener;
   int _timerValue = 0;
+
+  HomeViewModel() {}
 
   set timerValue(int value) {
     _timerValue = value;
@@ -19,10 +25,33 @@ class HomeViewModel extends Model {
   void startTimer() {
     pageController.nextPage(
         duration: _pageChangeDuration, curve: _pageChangeCurve);
+    var seconds = _timerValue * 60;
+    var secondsStream = Stream.periodic(Duration(seconds: 1))
+        .map((timePassed) => seconds - timePassed);
+    _updateSecondsStream(secondsStream);
   }
 
   void stopTimer() {
+    _removeSecondsListener();
     pageController.previousPage(
         duration: _pageChangeDuration, curve: _pageChangeCurve);
+  }
+
+  void _updateSecondsStream(Stream<int> secondsStream) {
+    this.secondsStream = secondsStream.asBroadcastStream();
+    _removeSecondsListener();
+    _secondsListener = this.secondsStream.listen((seconds) {
+      if (seconds == 0) {
+        stopTimer();
+      }
+    });
+    notifyListeners();
+  }
+
+  void _removeSecondsListener() {
+    if (_secondsListener != null) {
+      _secondsListener.cancel();
+      _secondsListener = null;
+    }
   }
 }
--- lib/src/timer/timer_widget.dart
+++ lib/src/timer/timer_widget.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/src/home_view_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class TimerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
-        return Text(
-          '4:31',
-          style: textTheme.display4.copyWith(color: Colors.black),
+    return ScopedModelDescendant<HomeViewModel>(
+      builder: (context, _, viewModel) {
+        return StreamBuilder(
+          stream: viewModel.secondsStream,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Text('loading');
+            }
+            return Text(
+              '${snapshot.data}',
+              style: textTheme.display4.copyWith(color: Colors.black),
+            );
+          },
+        );
+      },
     );
   }
 }
